33e05f1896dcd3803df64bfeed612f570c604d07
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-05-15 10:02:01 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+let resultado;
+
+num1 = Number(window.prompt("digite aqui o primeiro numero"));
+num2 = Number(window.prompt("digite o segundo numero"));
+
+resultado = num1 + num2; 
+
+alert(resultado);
