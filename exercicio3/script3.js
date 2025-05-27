4024e08615ed3523285e719302409dc0e80c5a38
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-05-27 08:44:27 -0300

    Add files via upload

---
+++ exercicio3/script3.js
@@ -0,0 +1,8 @@
+let num1 = Number (window.prompt ("digite aqui a primeira nota"));
+let num2 = Number (window.prompt ("digite aqui a segunda nota"));
+let num3 = Number (window.prompt ("digite aqui a terceira nota")); 
+
+let menoroumaior; 
+
+
+
