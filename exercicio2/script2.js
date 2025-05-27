ee007c9ccece7be6be1a52d06475e0b72149d448
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-05-27 12:23:56 -0300

    Add files via upload

--- exercicio2/script2.js
+++ exercicio2/script2.js
@@ -5,5 +5,31 @@ function somar(){
     let resultado = num1 + num2;
     alert("o resultado é" + resultado + ".")
 
-    
+}
+
+function subtrair(){
+    let num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let num2 = Number(window.prompt("digite aqui o segundo numero"));
+
+    let resultado = num1 - num2;
+
+    alert("o resultado é" + resultado + ".");
+}
+
+function multiplicar(){
+    let num1 = Number(window.prompt("digite aqui o primeiro numero: "));
+    let num2 = Number(window.prompt("digite aqui o segundo numero: "));
+
+    let resultado = num1 * num2;
+
+    alert("o resultado é " + resultado + ".")
+}
+
+function dividir(){
+    let num1 = Number(window.prompt("digite aqui o primeiro numero: "));
+    let num2 = Number(window.prompt("digite aqui o segundo numero:"));
+
+    let resultado = num1 / num2;
+
+    alert("o resultado é " + resultado + ".")
 }
--- exercicio3/script3.js
+++ exercicio3/script3.js
@@ -1,8 +1,6 @@
 let num1 = Number (window.prompt ("digite aqui a primeira nota"));
 let num2 = Number (window.prompt ("digite aqui a segunda nota"));
 let num3 = Number (window.prompt ("digite aqui a terceira nota")); 
+let resultado 
 
-let menoroumaior; 
-
-
-
+resultado = num1 + num2 + num3
