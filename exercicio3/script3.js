4f1b5eac791b8755a09498a4ccac377f4407762c
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-06-05 10:48:58 -0300

    Add files via upload

--- exercicio3/script3.js
+++ exercicio3/script3.js
@@ -1,8 +1,19 @@
-let num1 = Number (window.prompt ("digite aqui a primeira nota"));
-let num2 = Number (window.prompt ("digite aqui a segunda nota"));
-let num3 = Number (window.prompt ("digite aqui a terceira nota")); 
-let resultado
+let nota1 = Number (window.prompt ("digite aqui a primeira nota"));
+let nota2 = Number (window.prompt ("digite aqui a segunda nota"));
+let nota3 = Number (window.prompt ("digite aqui a terceira nota")); 
+let pontosfaltando; 
 
-resultado = num1 + num2 + num3
+let total= (nota1 + nota2 + nota3)/3;
 
-nota
+alura = "alura"
+
+  let resultado = nota1 + nota2 + nota3;
+    alert("a média é" + resultado + ".")
+
+if(total >= 60){
+    alert("voce atingiu a média" + total)
+    alert("aprovado")
+} else {
+    pontosfaltando = 60 - total 
+    alert(nome + "reprovado , faltam" + pontosfaltando + "pontos, estuda ai: ")
+}
