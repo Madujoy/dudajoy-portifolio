96b508fe3801d8395d989b7ea48bc551d6703598
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-06-10 12:23:33 -0300

    Add files via upload

--- exercicio5/script5.js
+++ exercicio5/script5.js
@@ -1,20 +1,11 @@
 function calcular() {
-     num1 = Number(window.prompt("digite aqui o primeiro numero"));
-     num2 = Number(window.prompt("digite o segundo numero"));
-let num1;
-let num2;
-let resultado;
-let maioroumenor;
+num1 = Number(window.prompt("digite aqui o primeiro numero"));
+num2 = Number(window.prompt("digite o segundo numero"));
 
 
-resultado = num1 + num2; 
-
-maioroumenor = resultado % 2; 
-
-if( maioroumenor ==0) {
+if( num1 > num2) {
      alert(" o numero " + num1 + " é maior");
 } else {
      alert(" o numero " + num2 + "é maior");
-     alert(" o numero " + num1 + "é menor")
 }
 }
