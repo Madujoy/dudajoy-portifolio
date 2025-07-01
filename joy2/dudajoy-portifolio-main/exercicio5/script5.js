972675da9743a2ed68af6c662fb3b681c3486f2e
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-07-01 12:26:30 -0300

    Add files via upload

---
+++ joy2/dudajoy-portifolio-main/exercicio5/script5.js
@@ -0,0 +1,11 @@
+function calcular() {
+num1 = Number(window.prompt("digite aqui o primeiro numero"));
+num2 = Number(window.prompt("digite o segundo numero"));
+
+
+if( num1 > num2) {
+     alert(" o numero " + num1 + " é maior");
+} else {
+     alert(" o numero " + num2 + "é maior");
+}
+}
