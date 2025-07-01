f2f2e7b9867b0fdbdaf340abf4fb888d09753057
Duda <alves.andrade.maria@escola.pr.gov.br>
2025-07-01 10:49:06 -0300

    Add files via upload

---
+++ joy/dudajoy-portifolio-main/exercicio10/script10.js
@@ -0,0 +1,13 @@
+function contar (){
+    let mostraHTML = document.getElementById("mostraHTML");
+
+    let contador = 1;
+
+    while (contador <=10) {
+        mostraHTML.innerHTML += " ${contador} &#128570 ";
+        contador++;
+    }
+
+    mostraHTML.innerHTML += '&#128568;';
+
+}
